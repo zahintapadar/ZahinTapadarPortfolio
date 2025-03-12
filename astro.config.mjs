84e400eb9437174c29029ce2db405edf7931ee66
Gothsec <oscarandreshernandezpineda@gmail.com>
2025-03-12 16:24:54 -0500

    Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,6 @@ import react from '@astrojs/react';
 export default defineConfig({
   integrations: [tailwind(), react()],
   vite: {
-    optimizeDeps: {
-      include: ["zwitch"]
-    },
     resolve: {
       alias: {
         '@': '/src',
