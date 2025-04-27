79ecededb58c754de578ea5dd6c35c3c11b546d9
Zahin Tapadar <eufalkon@gmail.com>
2025-04-28 00:06:17 +0530

    first commit

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,14 +1,21 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
-  authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDqouZXgw8w8ZtqEPQHU90XgFlsd5L9_CI",
+  authDomain: "zahintapadar-portfolio.firebaseapp.com",
+  projectId: "zahintapadar-portfolio",
+  storageBucket: "zahintapadar-portfolio.firebasestorage.app",
+  messagingSenderId: "376679271214",
+  appId: "1:376679271214:web:330cdc14325c9aacfc08bf",
+  measurementId: "G-RGHK9JGQ98"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
